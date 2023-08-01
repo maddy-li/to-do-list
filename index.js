0d95f410764387cf2161dfe8cc7d6e455cae0c8a
Maddy Li <md5013@nyu.edu>
2023-07-31 22:27:43 -0400

    added dynamic time display

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+app.set("view engine", "ejs");
+
 app.use('/public', express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
