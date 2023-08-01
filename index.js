30952b29d85519e6bbe2c012f09bce0399049991
Maddy Li <md5013@nyu.edu>
2023-07-31 22:39:56 -0400

    added functioning to-do list input (no styling)

--- index.js
+++ index.js
@@ -9,8 +9,16 @@ app.set("view engine", "ejs");
 app.use('/public', express.static('public'));
 app.use(bodyParser.urlencoded({extended: true}));
 
+const tasks = [];
+
 app.get("/", (req, res) => {
-    res.render("index.ejs");
+    res.render("index.ejs", { tasks: tasks }); 
+});
+
+app.post("/addTask", (req, res) => {
+    const { task } = req.body;
+    tasks.push(task);
+    res.redirect("/");
 })
 
 app.listen(port, () => {
